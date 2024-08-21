fc26121a9eba8ea583e40f13da67af1524264871
Tchano-Py <tchanopy@gmail.com>
2024-08-21 16:43:01 +0100

    feat: adicionar flick_video_player e video_player com testes iniciais para vídeos locais e da web

--- lib/views/ao_vivo_page/page.dart
+++ lib/views/ao_vivo_page/page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:video_player/video_player.dart';
 
 class PageAoVivo extends StatefulWidget {
   const PageAoVivo({super.key});
@@ -8,15 +10,59 @@ class PageAoVivo extends StatefulWidget {
 }
 
 class _PageAoVivoState extends State<PageAoVivo> {
+  late FlickManager _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = FlickManager(
+      videoPlayerController: VideoPlayerController.asset(
+        'assets/video/video_1.mp4',
+      )..initialize().then(
+          (_) {
+            setState(() {});
+          },
+        ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(top: 30),
-      child: const SingleChildScrollView(
-        child: Center(
-          child: Text('Ao Vivo'),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  FlickVideoPlayer(
+                    flickManager: _controller,
+                  ),
+                  if (_controller.flickVideoManager?.videoPlayerController
+                          ?.value.isInitialized ==
+                      false)
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Image.asset('assets/image/noticia-destaque.png'),
+                        const CircularProgressIndicator(),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
