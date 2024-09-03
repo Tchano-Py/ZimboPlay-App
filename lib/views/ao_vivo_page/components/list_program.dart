20495a40f74483f0ec0cba89133a8cb4ea343c23
Tchano-Py <tchanopy@gmail.com>
2024-09-03 10:03:11 +0100

    fix: adiciona animação na página "Ao Vivo" para melhor experiência de entrada
    - Implementada animação na página "Ao Vivo" utilizando a biblioteca Animate_do.
    - Ajuste dinâmico dos tempos de animação para tornar a transição dos elementos mais suave.
    - Melhoria na apresentação dos itens da página, oferecendo uma experiência de visualização mais envolvente para o usuário.

--- lib/views/ao_vivo_page/components/list_program.dart
+++ lib/views/ao_vivo_page/components/list_program.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 class ListProgram extends StatefulWidget {
   const ListProgram({super.key});
@@ -8,6 +9,8 @@ class ListProgram extends StatefulWidget {
 }
 
 class _ListProgramState extends State<ListProgram> {
+  bool animate = true;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -15,45 +18,66 @@ class _ListProgramState extends State<ListProgram> {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
-          SizedBox(
-            width: 99.0,
-            height: 99.0,
-            child: Card(
-              color: const Color.fromRGBO(34, 34, 34, 1),
-              clipBehavior: Clip.antiAlias,
-              child: Image.asset('assets/image/programa_1.png'),
+          FadeInUp(
+            animate: animate,
+            duration: const Duration(milliseconds: 1100),
+            from: 0,
+            delay: const Duration(seconds: 0),
+            child: SizedBox(
+              width: 99.0,
+              height: 99.0,
+              child: Card(
+                color: const Color.fromRGBO(34, 34, 34, 1),
+                clipBehavior: Clip.antiAlias,
+                child: Image.asset('assets/image/programa_1.png'),
+              ),
             ),
           ),
           const SizedBox(width: 10),
-          SizedBox(
-            width: 99.0,
-            height: 99.0,
-            child: Card(
-              color: const Color.fromRGBO(34, 34, 34, 1),
-              clipBehavior: Clip.antiAlias,
-              child: Image.asset('assets/image/programa_2.png'),
+          FadeInUp(
+            animate: animate,
+            duration: const Duration(milliseconds: 1100),
+            from: 50,
+            child: SizedBox(
+              width: 99.0,
+              height: 99.0,
+              child: Card(
+                color: const Color.fromRGBO(34, 34, 34, 1),
+                clipBehavior: Clip.antiAlias,
+                child: Image.asset('assets/image/programa_2.png'),
+              ),
             ),
           ),
           const SizedBox(width: 10),
-          SizedBox(
-            width: 99.0,
-            height: 99.0,
-            child: Card(
-              color: const Color.fromRGBO(34, 34, 34, 1),
-              clipBehavior: Clip.antiAlias,
-              child: Image.asset('assets/image/programa_3.png'),
+          FadeInUp(
+            animate: animate,
+            duration: const Duration(milliseconds: 1100),
+            from: 100,
+            child: SizedBox(
+              width: 99.0,
+              height: 99.0,
+              child: Card(
+                color: const Color.fromRGBO(34, 34, 34, 1),
+                clipBehavior: Clip.antiAlias,
+                child: Image.asset('assets/image/programa_3.png'),
+              ),
             ),
           ),
           const SizedBox(width: 10),
-          SizedBox(
-            width: 99.0,
-            height: 99.0,
-            child: Card(
-              color: const Color.fromRGBO(34, 34, 34, 1),
-              clipBehavior: Clip.antiAlias,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Image.asset('assets/image/programa_4.png'),
+          FadeInUp(
+            animate: animate,
+            duration: const Duration(milliseconds: 1100),
+            from: 150,
+            child: SizedBox(
+              width: 99.0,
+              height: 99.0,
+              child: Card(
+                color: const Color.fromRGBO(34, 34, 34, 1),
+                clipBehavior: Clip.antiAlias,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.asset('assets/image/programa_4.png'),
+                ),
               ),
             ),
           ),
--- lib/views/ao_vivo_page/page.dart
+++ lib/views/ao_vivo_page/page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flick_video_player/flick_video_player.dart';
 import 'package:video_player/video_player.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -16,6 +17,7 @@ class PageAoVivo extends StatefulWidget {
 
 class _PageAoVivoState extends State<PageAoVivo> {
   late FlickManager _controller;
+  bool animate = true;
 
   //Salvar o tempo do video(onde parai de ver)
   void _saveVideoPosition(Duration position) async {
@@ -65,147 +67,179 @@ class _PageAoVivoState extends State<PageAoVivo> {
                 ),
                 color: Colors.white,
               ),
-              child: Column(
-                children: <Widget>[
-                  AspectRatio(
-                    aspectRatio: 16 / 10,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
+              child: FadeInUp(
+                animate: animate,
+                duration: const Duration(milliseconds: 500),
+                delay: const Duration(seconds: 0),
+                child: Column(
+                  children: <Widget>[
+                    FadeIn(
+                      animate: animate,
+                      duration: const Duration(milliseconds: 600),
+                      delay: const Duration(seconds: 0),
+                      child: AspectRatio(
+                        aspectRatio: 16 / 10,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                            ),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              FlickVideoPlayer(
+                                flickManager: _controller,
+                              ),
+                              if (_controller
+                                      .flickVideoManager
+                                      ?.videoPlayerController
+                                      ?.value
+                                      .isInitialized ==
+                                  false)
+                                Stack(
+                                  fit: StackFit.expand,
+                                  children: [
+                                    Image.asset(
+                                      'assets/image/fundo_video_1.jpeg',
+                                      fit: BoxFit.cover,
+                                    ),
+                                    BackdropFilter(
+                                      filter: ImageFilter.blur(
+                                        sigmaX: 4,
+                                        sigmaY: 4,
+                                      ),
+                                      child: Container(
+                                        color: const Color.fromRGBO(
+                                            34, 34, 34, .4),
+                                      ),
+                                    ),
+                                    const Align(
+                                      alignment: Alignment.center,
+                                      child: SizedBox(
+                                        height: 30,
+                                        width: 30,
+                                        child: CircularProgressIndicator(
+                                          color:
+                                              Color.fromRGBO(196, 144, 33, 1),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                            ],
+                          ),
                         ),
                       ),
-                      clipBehavior: Clip.antiAlias,
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          FlickVideoPlayer(
-                            flickManager: _controller,
-                          ),
-                          if (_controller
-                                  .flickVideoManager
-                                  ?.videoPlayerController
-                                  ?.value
-                                  .isInitialized ==
-                              false)
-                            Stack(
-                              fit: StackFit.expand,
-                              children: [
-                                Image.asset(
-                                  'assets/image/fundo_video_1.jpeg',
-                                  fit: BoxFit.cover,
-                                ),
-                                BackdropFilter(
-                                  filter: ImageFilter.blur(
-                                    sigmaX: 4,
-                                    sigmaY: 4,
-                                  ),
-                                  child: Container(
-                                    color: const Color.fromRGBO(34, 34, 34, .4),
-                                  ),
-                                ),
-                                const Align(
-                                  alignment: Alignment.center,
-                                  child: SizedBox(
-                                    height: 30,
-                                    width: 30,
-                                    child: CircularProgressIndicator(
-                                      color: Color.fromRGBO(196, 144, 33, 1),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          FadeInDown(
+                            animate: animate,
+                            duration: const Duration(milliseconds: 700),
+                            delay: const Duration(seconds: 0),
+                            child: Row(
+                              children: <Widget>[
+                                const Expanded(
+                                  child: Text(
+                                    "Sexto Sentido",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 20,
+                                      color: Color.fromRGBO(115, 117, 84, 1),
                                     ),
+                                  ),
+                                ),
+                                Image.asset("assets/image/eye.png"),
+                                const SizedBox(width: 8),
+                                const Text(
+                                  "+17",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 18,
+                                    color: Color.fromRGBO(196, 144, 33, 1),
                                   ),
                                 ),
                               ],
                             ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 6,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            const Expanded(
-                              child: Text(
-                                "Sexto Sentido",
+                          ),
+                          const SizedBox(height: 6),
+                          FadeInDown(
+                            animate: animate,
+                            duration: const Duration(milliseconds: 800),
+                            delay: const Duration(seconds: 0),
+                            child: const Expandabletext(
+                              maxLines: 3,
+                              text:
+                                  "É um programa audiovisual, da grelha de programação da estação televisiva TV Zimbo, que vai ao ar em direto, as 09 horas, e tem duração de 2 horas e 35 minutos, apresentado pela carismatica, Dina Simão, conta com companhia da  Mimi (assistente de estudio), que esta sempre bem humorada, disposta a  contagiar com a sua alegria os convidados e os telespectadores. O programa matinal da TV Zimbo (Sexto Sentido), conta ainda com bons convidados, surpresas, e rubricas, que não vais querer perder.",
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 13,
+                                color: Color.fromRGBO(115, 115, 84, 1),
+                              ),
+                              fadedTextStyle: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 13,
+                                color: Color.fromRGBO(115, 115, 84, 0.0),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          FadeInDown(
+                            animate: animate,
+                            duration: const Duration(milliseconds: 900),
+                            delay: const Duration(seconds: 0),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                backgroundColor:
+                                    const Color.fromRGBO(196, 144, 33, 1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                minimumSize:
+                                    Size(MediaQuery.of(context).size.width, 38),
+                              ),
+                              child: const Text(
+                                "Ver agenda do programa",
                                 style: TextStyle(
                                   fontWeight: FontWeight.w700,
-                                  fontSize: 20,
-                                  color: Color.fromRGBO(115, 117, 84, 1),
+                                  fontSize: 18,
+                                  color: Colors.white,
                                 ),
                               ),
                             ),
-                            Image.asset("assets/image/eye.png"),
-                            const SizedBox(width: 8),
-                            const Text(
-                              "+17",
+                          ),
+                          const SizedBox(height: 26),
+                          FadeInDown(
+                            animate: animate,
+                            duration: const Duration(milliseconds: 1000),
+                            delay: const Duration(seconds: 0),
+                            child: const Text(
+                              "Programação completa",
                               style: TextStyle(
                                 fontWeight: FontWeight.w700,
-                                fontSize: 18,
-                                color: Color.fromRGBO(196, 144, 33, 1),
+                                fontSize: 20,
+                                color: Color.fromRGBO(34, 34, 34, 1),
                               ),
                             ),
-                          ],
-                        ),
-                        const SizedBox(height: 6),
-                        const Expandabletext(
-                          maxLines: 3,
-                          text:
-                              "É um programa audiovisual, da grelha de programação da estação televisiva TV Zimbo, que vai ao ar em direto, as 09 horas, e tem duração de 2 horas e 35 minutos, apresentado pela carismatica, Dina Simão, conta com companhia da  Mimi (assistente de estudio), que esta sempre bem humorada, disposta a  contagiar com a sua alegria os convidados e os telespectadores. O programa matinal da TV Zimbo (Sexto Sentido), conta ainda com bons convidados, surpresas, e rubricas, que não vais querer perder.",
-                          textStyle: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 13,
-                            color: Color.fromRGBO(115, 115, 84, 1),
                           ),
-                          fadedTextStyle: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 13,
-                            color: Color.fromRGBO(115, 115, 84, 0.0),
-                          ),
-                        ),
-                        const SizedBox(height: 4),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            backgroundColor:
-                                const Color.fromRGBO(196, 144, 33, 1),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            minimumSize:
-                                Size(MediaQuery.of(context).size.width, 38),
-                          ),
-                          child: const Text(
-                            "Ver agenda do programa",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 18,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 26),
-                        const Text(
-                          "Programação completa",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                            color: Color.fromRGBO(34, 34, 34, 1),
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        const ListProgram()
-                      ],
+                          const SizedBox(height: 12),
+                          const ListProgram()
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
